afcf0cc133d428fc194028766e25e901cec03699
javisar <javisar@gmail.com>
2018-07-17 13:57:27 +0200

    More test

--- Source/AirLockMod/AirLockConfig.cs
+++ Source/AirLockMod/AirLockConfig.cs
@@ -6,17 +6,17 @@ using UnityEngine;
 public class AirLockConfig : IBuildingConfig
 {
 	public const string ID = "AirLock";
-
+	/*
 	public static readonly LogicPorts.Port[] INPUT_PORTS = new LogicPorts.Port[1]
 	{
 		LogicPorts.Port.InputPort(Door.OPEN_CLOSE_PORT_ID, new CellOffset(0, 0), STRINGS.BUILDINGS.PREFABS.DOOR.LOGIC_PORT_DESC, false)
 	};
-
+	*/
 	public override BuildingDef CreateBuildingDef()
 	{
 		int width = 1;
 		int height = 2;
-		string anim = "door_internal_kanim";
+		string anim = "door_manual_kanim";
 		int hitpoints = 30;
 		float construction_time = 10f;
 		float[] tIER = TUNING.BUILDINGS.CONSTRUCTION_MASS_KG.TIER2;
@@ -25,6 +25,8 @@ public class AirLockConfig : IBuildingConfig
 		BuildLocationRule build_location_rule = BuildLocationRule.Tile;
 		EffectorValues nONE = NOISE_POLLUTION.NONE;
 		BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(ID, width, height, anim, hitpoints, construction_time, tIER, aLL_METALS, melting_point, build_location_rule, TUNING.BUILDINGS.DECOR.NONE, nONE, 1f);
+		buildingDef.Overheatable = false;
+		buildingDef.Floodable = false;
 		buildingDef.Entombable = false;
 		buildingDef.IsFoundation = true;
 		buildingDef.MaterialCategory = MATERIALS.ALL_METALS;
@@ -32,35 +34,32 @@ public class AirLockConfig : IBuildingConfig
 		buildingDef.PermittedRotations = PermittedRotations.R90;
 		buildingDef.SceneLayer = Grid.SceneLayer.TileMain;
 		buildingDef.TileLayer = ObjectLayer.FoundationTile;
-		SoundEventVolumeCache.instance.AddVolume("door_internal_kanim", "Open_DoorInternal", NOISE_POLLUTION.NOISY.TIER2);
-		SoundEventVolumeCache.instance.AddVolume("door_internal_kanim", "Close_DoorInternal", NOISE_POLLUTION.NOISY.TIER2);
+		SoundEventVolumeCache.instance.AddVolume("door_manual_kanim", "ManualPressureDoor_gear_LP", NOISE_POLLUTION.NOISY.TIER1);
+		SoundEventVolumeCache.instance.AddVolume("door_manual_kanim", "ManualPressureDoor_open", NOISE_POLLUTION.NOISY.TIER2);
+		SoundEventVolumeCache.instance.AddVolume("door_manual_kanim", "ManualPressureDoor_close", NOISE_POLLUTION.NOISY.TIER2);
 		return buildingDef;
 	}
 
-	public override void DoPostConfigurePreview(BuildingDef def, GameObject go)
+	public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
 	{
-		GeneratedBuildings.RegisterLogicPorts(go, DoorConfig.INPUT_PORTS);
-	}
-
-	public override void DoPostConfigureUnderConstruction(GameObject go)
-	{
-		GeneratedBuildings.RegisterLogicPorts(go, DoorConfig.INPUT_PORTS);
+		Door door = go.AddOrGet<Door>();
+		door.hasComplexUserControls = true;
+		door.unpoweredAnimSpeed = 1f;
+		door.doorType = Door.DoorType.ManualPressure;
+		go.AddOrGet<AccessControl>();
+		go.AddOrGet<KBoxCollider2D>();
+		Prioritizable.AddRef(go);
+		Workable workable = go.AddOrGet<Workable>();
+		workable.workTime = 5f;
+		Object.DestroyImmediate(go.GetComponent<BuildingEnabledButton>());
 	}
 
 	public override void DoPostConfigureComplete(GameObject go)
 	{
-		AirLockDoor door = go.AddOrGet<AirLockDoor>();
-		door.unpoweredAnimSpeed = 1f;
-		door.doorType = AirLockDoor.DoorType.Internal;
-		AccessControl accessControl = go.AddOrGet<AccessControl>();
-		accessControl.controlEnabled = true;
-		Workable workable = go.AddOrGet<Workable>();
-		workable.workTime = 3f;
-		KBatchedAnimController component = go.GetComponent<KBatchedAnimController>();
-		component.initialAnim = "closed";
-		go.AddOrGet<KBoxCollider2D>();
-		Prioritizable.AddRef(go);
-		GeneratedBuildings.RegisterLogicPorts(go, DoorConfig.INPUT_PORTS);
-		Object.DestroyImmediate(go.GetComponent<BuildingEnabledButton>());
+		BuildingTemplates.DoPostConfigure(go);
+		AccessControl component = go.GetComponent<AccessControl>();
+		component.controlEnabled = true;
+		KBatchedAnimController component2 = go.GetComponent<KBatchedAnimController>();
+		component2.initialAnim = "closed";
 	}
 }
