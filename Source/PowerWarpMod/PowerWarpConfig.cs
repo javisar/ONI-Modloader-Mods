87ca46e35e355d6570d46a990dc2305a67257da3
javisar <javisar@gmail.com>
2018-08-05 19:01:44 +0200

    PowerWarp first version

---
+++ Source/PowerWarpMod/PowerWarpConfig.cs
@@ -0,0 +1,80 @@
+﻿// PowerTransformerSmallConfig
+using TUNING;
+using UnityEngine;
+
+public class PowerWarpConfig : IBuildingConfig
+{
+    public const string ID = "PowerWarp";
+
+    public override BuildingDef CreateBuildingDef()
+    {
+        string id = "PowerWarp";
+        int width = 2;
+        int height = 2;
+        string anim = "transformer_small_kanim";
+        int hitpoints = 30;
+        float construction_time = 30f;
+        float[] tIER = BUILDINGS.CONSTRUCTION_MASS_KG.TIER3;
+        string[] rAW_METALS = MATERIALS.RAW_METALS;
+        float melting_point = 800f;
+        BuildLocationRule build_location_rule = BuildLocationRule.OnFloor;
+        EffectorValues tIER2 = NOISE_POLLUTION.NOISY.TIER5;
+        BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(id, width, height, anim, hitpoints, construction_time, tIER, rAW_METALS, melting_point, build_location_rule, BUILDINGS.DECOR.PENALTY.TIER1, tIER2, 0.2f);
+        buildingDef.RequiresPowerInput = true;
+        buildingDef.UseWhitePowerOutputConnectorColour = true;
+        buildingDef.PowerInputOffset = new CellOffset(0, 1);
+        buildingDef.PowerOutputOffset = new CellOffset(1, 0);
+        buildingDef.ElectricalArrowOffset = new CellOffset(1, 0);
+        buildingDef.ExhaustKilowattsWhenActive = 0.25f;
+        buildingDef.SelfHeatKilowattsWhenActive = 1f;
+        buildingDef.ViewMode = SimViewMode.PowerMap;
+        buildingDef.AudioCategory = "Metal";
+        buildingDef.ExhaustKilowattsWhenActive = 0f;
+        buildingDef.SelfHeatKilowattsWhenActive = 1f;
+        buildingDef.Entombable = true;
+        buildingDef.GeneratorWattageRating = 1000f;
+        buildingDef.GeneratorBaseCapacity = 1000f;
+        buildingDef.PermittedRotations = PermittedRotations.FlipH;
+        return buildingDef;
+    }
+
+    public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
+    {
+        go.GetComponent<KPrefabID>().AddPrefabTag(RoomConstraints.ConstraintTags.IndustrialMachinery);
+        go.AddComponent<RequireInputs>();
+        BuildingDef def = go.GetComponent<Building>().Def;
+        Battery battery = go.AddOrGet<Battery>();
+        battery.powerSortOrder = 1000;
+        battery.capacity = def.GeneratorWattageRating;
+        battery.chargeWattage = def.GeneratorWattageRating;
+        PowerTransformer powerTransformer = go.AddComponent<PowerTransformer>();
+        powerTransformer.powerDistributionOrder = 9;
+
+        ValveBase valveBase = go.AddOrGet<ValveBase>();
+        //valveBase.conduitType = ConduitType.Liquid;
+        valveBase.conduitType = (ConduitType)102;
+        valveBase.maxFlow = 10f;
+        valveBase.animFlowRanges = new ValveBase.AnimRangeInfo[3]
+        {
+            new ValveBase.AnimRangeInfo(3f, "lo"),
+            new ValveBase.AnimRangeInfo(7f, "med"),
+            new ValveBase.AnimRangeInfo(10f, "hi")
+        };
+
+        go.AddOrGet<Valve>();
+        Workable workable = go.AddOrGet<Workable>();
+        workable.workTime = 5f;
+    }
+
+    public override void DoPostConfigureComplete(GameObject go)
+    {
+        Object.DestroyImmediate(go.GetComponent<EnergyConsumer>());
+        Object.DestroyImmediate(go.GetComponent<RequireInputs>());
+        BuildingTemplates.DoPostConfigure(go);
+        go.GetComponent<KPrefabID>().prefabInitFn += delegate (GameObject game_object)
+        {
+            PoweredActiveController.Instance instance = new PoweredActiveController.Instance(game_object.GetComponent<KPrefabID>());
+            instance.StartSM();
+        };
+    }
+}
---
+++ Source/PowerWarpMod/PowerWarpMod.cs
@@ -0,0 +1,136 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PowerWarpMod
+{
+
+    internal class PowerWarpMod_Utils
+    {
+        public static ConduitType GetConduitType(ValveSideScreen __instance)
+        {
+            FieldInfo fi1 = AccessTools.Field(typeof(ValveSideScreen), "targetValve");
+            FieldInfo fi2 = AccessTools.Field(typeof(Valve), "valveBase");
+
+            ConduitType type = ((ValveBase)fi2.GetValue(fi1.GetValue(__instance))).conduitType;
+            return type;
+        }
+    }
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    internal class PowerWarpMod_GeneratedBuildings_LoadGeneratedBuildings
+    {
+        private static void Prefix()
+        {
+            Debug.Log(" === PowerWarpMod_GeneratedBuildings_LoadGeneratedBuildings Prefix === " + PowerWarpConfig.ID);
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.POWERWARP.NAME", "Power Stargate");
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.POWERWARP.DESC", "Power Stargates provides an easy way to transport power from one place to another.");
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.POWERWARP.EFFECT", "Place one providing input wire, and another one with an output wire. Sintonize your stargates using the same channel.");
+
+            List<string> ls = new List<string>((string[])TUNING.BUILDINGS.PLANORDER[10].data);
+            ls.Add(PowerWarpConfig.ID);
+            TUNING.BUILDINGS.PLANORDER[10].data = (string[])ls.ToArray();
+
+            TUNING.BUILDINGS.COMPONENT_DESCRIPTION_ORDER.Add(PowerWarpConfig.ID);
+
+
+        }
+        private static void Postfix()
+        {
+
+            Debug.Log(" === PowerWarpMod_GeneratedBuildings_LoadGeneratedBuildings Postfix === " + PowerWarpConfig.ID);
+            object obj = Activator.CreateInstance(typeof(PowerWarpConfig));
+            BuildingConfigManager.Instance.RegisterBuilding(obj as IBuildingConfig);
+        }
+    }
+
+    [HarmonyPatch(typeof(Db), "Initialize")]
+    internal class PowerWarpMod_Db_Initialize
+    {
+        private static void Prefix(Db __instance)
+        {
+            Debug.Log(" === PowerWarpMod_Db_Initialize loaded === " + PowerWarpConfig.ID);
+            List<string> ls = new List<string>((string[])Database.Techs.TECH_GROUPING["AdvancedPowerRegulation"]);
+            ls.Add(PowerWarpConfig.ID);
+            Database.Techs.TECH_GROUPING["AdvancedPowerRegulation"] = (string[])ls.ToArray();            
+        }
+    }
+
+    [HarmonyPatch(typeof(ValveBase), "ConduitUpdate")]
+    internal class PowerWarpMod_ValveBase_ConduitUpdate
+    {
+        private static bool Prefix(ValveBase __instance, float dt)
+        {
+            
+
+
+            return true;
+        }
+    }
+
+
+    [HarmonyPatch(typeof(ValveSideScreen), "OnSpawn")]
+    internal class PowerWarpMod_ValveSideScreen_OnSpawn
+    {
+        private static void Postfix(ValveSideScreen __instance)
+        {
+            Debug.Log(" === PowerWarpMod_ValveSideScreen_OnSpawn Postfix === ");
+
+            FieldInfo fi0 = AccessTools.Field(typeof(ValveSideScreen), "unitsLabel");
+            ConduitType type = PowerWarpMod_Utils.GetConduitType(__instance);
+            if (type == (ConduitType)102)
+            {
+                ((LocText)fi0.GetValue(__instance)).text = "Ch.";
+            }
+
+        }
+
+    }
+
+    [HarmonyPatch(typeof(SideScreenContent), "GetTitle")]
+    internal class PowerWarpMod_SideScreenContent_GetTitle
+    {
+        private static bool Prefix(SideScreenContent __instance, ref string __result)
+        {
+            Debug.Log(" === PowerWarpMod_SideScreenContent_GetTitle Postfix === ");
+
+            if (!(__instance is ValveSideScreen)) return true;
+
+            ConduitType type = PowerWarpMod_Utils.GetConduitType((ValveSideScreen)__instance);
+            if (type == (ConduitType)102)
+            {
+                __result = "Channel";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+    }
+
+    [HarmonyPatch(typeof(ValveSideScreen), "SetTarget")]
+    internal class PowerWarpMod_ValveSideScreen_SetTarget
+    {
+        private static void Postfix(ValveSideScreen __instance)
+        {
+            Debug.Log(" === PowerWarpMod_ValveSideScreen_SetTarget Postfix === ");
+
+            FieldInfo fi3 = AccessTools.Field(typeof(ValveSideScreen), "minFlowLabel");
+            FieldInfo fi4 = AccessTools.Field(typeof(ValveSideScreen), "maxFlowLabel");
+
+            ConduitType type = PowerWarpMod_Utils.GetConduitType(__instance);
+            if (type == (ConduitType)102)
+            {
+                ((LocText)fi3.GetValue(__instance)).text = "Ch.";
+                ((LocText)fi4.GetValue(__instance)).text = "Ch.";
+            }
+
+        }
+
+    }
+}
