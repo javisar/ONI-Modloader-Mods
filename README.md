26b7f15caa4eca8257fd3aeffe25fb380bd1eb98
javisar <javisar@gmail.com>
2018-08-06 11:22:26 +0200

    Cleaning and updated

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ Examples
 * InverseElectrolyzerMod: Combines hydrogen and oxygen into steam. Made by [@javisar](https://github.com/javisar).
 * LiquidTankMod: Storage for liquids. Made by [@javisar](https://github.com/javisar).
 * NaphthaViscosityMod: Recovers the old behaviour in Naphtha. Sets the viscosity to 0 and allows building vertical airlocks. Made by [@javisar](https://github.com/javisar).
-* NoFixedTemps: The output fluid temperatures of the machinery aren't fixed. Made by [@javisar](https://github.com/javisar).
+* NoFixedTemps: The output fluid temperatures of the machinery depends on the input (Except AirFilter, AlgaeTerraium and PacuCleaner). Made by [@javisar](https://github.com/javisar).
 * PressureDoorMod: Removes the energy need for the mechanized pressure door and makes it buildable from all material. Made by [@Killface1980](https://github.com/Killface1980).
 * RoomSizeMod: Increases the room size info tooltip from 128 to a maximun of 1024 tiles. Made by [@javisar](https://github.com/javisar).
 * SensorsMod: Allows for increased ranges in automation sensors (Made by [@fistak](https://github.com/fistak), adapted by [@Killface1980](https://github.com/Killface1980)).
--- Source/NoFixedTempMod/NoFixedTempMod.cs
+++ Source/NoFixedTempMod/NoFixedTempMod.cs
@@ -16,13 +16,16 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[2]
 			{
-				new ElementConverter.OutputElement(0.2f, SimHashes.Algae, 0f, true, 0f, 1f, true, 1f, 255, 0),
+                //new ElementConverter.OutputElement(0.2f, SimHashes.Algae, 303.15f, true, 0f, 1f, false, 1f, 255, 0),
+			    //new ElementConverter.OutputElement(0.400000036f, SimHashes.DirtyWater, 303.15f, true, 0f, 0.5f, false, 1f, 255, 0)
+
+                new ElementConverter.OutputElement(0.2f, SimHashes.Algae, 0f, true, 0f, 1f, true, 1f, 255, 0),
 				new ElementConverter.OutputElement(0.4f, SimHashes.DirtyWater, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
 			};
 		}
 	}
 
-
+    /*
 	[HarmonyPatch(typeof(AlgaeHabitatConfig), "ConfigureBuildingTemplate")]
 	internal class NoFixedTemp_AlgaeHabitatConfig_ConfigureBuildingTemplate
 	{
@@ -32,15 +35,18 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
-				new ElementConverter.OutputElement(0.0400000028f, SimHashes.Oxygen, 0f, false, 0f, 1f, true, 1f, 255, 0)
+                //new ElementConverter.OutputElement(0.0400000028f, SimHashes.Oxygen, 303.15f, false, 0f, 1f, false, 1f, 255, 0)
+                new ElementConverter.OutputElement(0.0400000028f, SimHashes.Oxygen, 0f, false, 0f, 1f, true, 1f, 255, 0)
 			};
 			ElementConverter elementConverter2 = go.AddComponent<ElementConverter>();
 			elementConverter2.outputElements = new ElementConverter.OutputElement[1]
 			{
-				new ElementConverter.OutputElement(0.290333331f, SimHashes.DirtyWater, 0f, true, 0f, 1f, true, 1f, 255, 0)
+                //new ElementConverter.OutputElement(0.290333331f, SimHashes.DirtyWater, 303.15f, true, 0f, 1f, false, 1f, 255, 0)
+                new ElementConverter.OutputElement(0.290333331f, SimHashes.DirtyWater, 0f, true, 0f, 1f, true, 1f, 255, 0)
 			};
 		}
 	}
+    */
 
 	[HarmonyPatch(typeof(CO2ScrubberConfig), "ConfigureBuildingTemplate")]
 	internal class NoFixedTemp_CO2ScrubberConfig_ConfigureBuildingTemplate
@@ -51,7 +57,8 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
-				new ElementConverter.OutputElement(1f, SimHashes.DirtyWater, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
+                //new ElementConverter.OutputElement(1f, SimHashes.DirtyWater, 313.15f, true, 0f, 0.5f, false, 1f, 255, 0)
+                new ElementConverter.OutputElement(1f, SimHashes.DirtyWater, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
 			};
 		}
 	}
@@ -65,6 +72,7 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
+                //new ElementConverter.OutputElement(0.1f, SimHashes.Dirt, 348.15f, true, 0f, 0.5f, false, 1f, 255, 0)
 				new ElementConverter.OutputElement(0.1f, SimHashes.Dirt, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
 			};
 		}
@@ -79,7 +87,10 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[2]
 			{
-				new ElementConverter.OutputElement(0.888f, SimHashes.Oxygen, 0f, false, 0f, 1f, true, 1f, 255, 0),
+                //new ElementConverter.OutputElement(0.888f, SimHashes.Oxygen, 343.15f, false, 0f, 1f, false, 1f, 255, 0),
+			    //new ElementConverter.OutputElement(0.111999989f, SimHashes.Hydrogen, 343.15f, false, 0f, 1f, false, 1f, 255, 0)
+
+                new ElementConverter.OutputElement(0.888f, SimHashes.Oxygen, 0f, false, 0f, 1f, true, 1f, 255, 0),
 				new ElementConverter.OutputElement(0.111999989f, SimHashes.Hydrogen, 0f, false, 0f, 1f, true, 1f, 255, 0)
 			};
 		}
@@ -94,7 +105,8 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
-				new ElementConverter.OutputElement(0.12f, SimHashes.Fertilizer, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
+                //new ElementConverter.OutputElement(0.12f, SimHashes.Fertilizer, 323.15f, true, 0f, 0.5f, false, 1f, 255, 0)
+                new ElementConverter.OutputElement(0.12f, SimHashes.Fertilizer, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
 			};
 		}
 	}
@@ -108,6 +120,7 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
+                //new ElementConverter.OutputElement(0.5f, SimHashes.Oxygen, 303.15f, false, 0f, 1f, false, 1f, 255, 0)
 				new ElementConverter.OutputElement(0.5f, SimHashes.Oxygen, 0f, false, 0f, 1f, true, 1f, 255, 0)
 			};
 		}
@@ -122,7 +135,10 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[2]
 			{
-				new ElementConverter.OutputElement(5f, SimHashes.Petroleum, 0f, true, 0f, 1f, true, 1f, 255, 0),
+                //new ElementConverter.OutputElement(5f, SimHashes.Petroleum, 348.15f, true, 0f, 1f, false, 1f, 255, 0),
+			    //new ElementConverter.OutputElement(0.09f, SimHashes.Methane, 348.15f, false, 0f, 3f, false, 1f, 255, 0)
+
+                new ElementConverter.OutputElement(5f, SimHashes.Petroleum, 0f, true, 0f, 1f, true, 1f, 255, 0),
 				new ElementConverter.OutputElement(0.09f, SimHashes.Methane, 0f, false, 0f, 3f, true, 1f, 255, 0)
 			};
 		}
@@ -137,11 +153,12 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
+                //new ElementConverter.OutputElement(3.33333325f, SimHashes.CrudeOil, 363.15f, false, 2f, 1.5f, false, 0f, 255, 0)
 				new ElementConverter.OutputElement(3.33333325f, SimHashes.CrudeOil, 0f, false, 2f, 1.5f, true, 0f, 255, 0)
 			};
 		}
 	}
-
+    /*
 	[HarmonyPatch(typeof(PacuCleanerConfig), "ConfigureBuildingTemplate")]
 	internal class NoFixedTemp_PacuCleanerConfig_ConfigureBuildingTemplate
 	{
@@ -151,11 +168,12 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
-				new ElementConverter.OutputElement(PacuCleanerConfig.POLLUTED_WATER_CONVERTED_PER_CYCLE / 600f, SimHashes.Water, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
+               
+                new ElementConverter.OutputElement(PacuCleanerConfig.POLLUTED_WATER_CONVERTED_PER_CYCLE / 600f, SimHashes.Water, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
 			};
 		}
 	}
-
+    */
 	[HarmonyPatch(typeof(PolymerizerConfig), "ConfigureBuildingTemplate")]
 	internal class NoFixedTemp_PolymerizerConfig_ConfigureBuildingTemplate
 	{
@@ -165,13 +183,18 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[3]
 			{
+                
+                //new ElementConverter.OutputElement(0.5f, SimHashes.Polypropylene, 348.15f, true, 0f, 0.5f, false, 1f, 255, 0),
+			    //new ElementConverter.OutputElement(0.008333334f, SimHashes.Steam, 473.15f, true, 0f, 0.5f, false, 1f, 255, 0),
+			    //new ElementConverter.OutputElement(0.008333334f, SimHashes.CarbonDioxide, 423.15f, true, 0f, 0.5f, false, 1f, 255, 0)
+                
 				new ElementConverter.OutputElement(0.5f, SimHashes.Polypropylene, 0f, true, 0f, 0.5f, true, 1f, 255, 0),
 				new ElementConverter.OutputElement(0.008333334f, SimHashes.Steam, 0f, true, 0f, 0.5f, true, 1f, 255, 0),
 				new ElementConverter.OutputElement(0.008333334f, SimHashes.CarbonDioxide, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
 			};
 		}
 	}
-
+    /*
 	[HarmonyPatch(typeof(ShowerConfig), "ConfigureBuildingTemplate")]
 	internal class NoFixedTemp_ShowerConfig_ConfigureBuildingTemplate
 	{
@@ -181,12 +204,15 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[1]
 			{
+                
+                //new ElementConverter.OutputElement(1f, SimHashes.DirtyWater, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
+                 
 				new ElementConverter.OutputElement(1f, SimHashes.DirtyWater, 0f, true, 0f, 0.5f, true, 1f, 255, 0)
 			};
 		}
 	}
-
-	[HarmonyPatch(typeof(WaterPurifierConfig), "ConfigureBuildingTemplate")]
+    */
+    [HarmonyPatch(typeof(WaterPurifierConfig), "ConfigureBuildingTemplate")]
 	internal class NoFixedTemp_SWaterPurifierConfig_ConfigureBuildingTemplate
 	{
 
@@ -195,8 +221,12 @@ namespace NoFixedTempMod
 			ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
 			elementConverter.outputElements = new ElementConverter.OutputElement[2]
 			{
+                
+                //new ElementConverter.OutputElement(5f, SimHashes.Water, 313.15f, true, 0f, 0.5f, false, 0.75f, 255, 0),
+			    //new ElementConverter.OutputElement(0.2f, SimHashes.ToxicSand, 313.15f, true, 0f, 0.5f, false, 0.25f, 255, 0)
+                
 				new ElementConverter.OutputElement(5f, SimHashes.Water, 0f, true, 0f, 0.5f, true, 0.75f, 255, 0),
-				new ElementConverter.OutputElement(0.2f, SimHashes.ToxicSand, 0f, true, 0f, 0.5f, true, 0.25f, 255, 0)
+				new ElementConverter.OutputElement(0.2f, SimHashes.ToxicSand, 0f, true, 0f, 0.5f, true, 0.25f, 255, 0)                
 			};
 		}
 	}
