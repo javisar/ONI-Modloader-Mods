90cc76ebd08ed3d3d344b2bf296fc7d8578d2d35
javisar <javisar@gmail.com>
2018-08-06 09:41:12 +0200

    Some more test

---
+++ Source/MultiWiresMod/MultiWireConfig.cs
@@ -0,0 +1,55 @@
+﻿// WireConfig
+using TUNING;
+using UnityEngine;
+
+public class MultiWireConfig : BaseWireConfig
+{
+    public const string ID = "MultiWire";
+
+    public override BuildingDef CreateBuildingDef()
+    {
+        string id = "MultiWire";
+        string anim = "utilities_electric_kanim";
+        float construction_time = 3f;
+        float[] tIER = BUILDINGS.CONSTRUCTION_MASS_KG.TIER0;
+        float insulation = 0.05f;
+        EffectorValues nONE = NOISE_POLLUTION.NONE;
+        BuildingDef buildingDef = base.CreateBuildingDef(id, anim, construction_time, tIER, insulation, BUILDINGS.DECOR.PENALTY.TIER0, nONE);
+        buildingDef.ViewMode = SimViewMode.Logic;
+        buildingDef.ObjectLayer = ObjectLayer.LogicWires;
+        buildingDef.TileLayer = ObjectLayer.LogicWiresTiling;
+        buildingDef.SceneLayer = Grid.SceneLayer.Wires;
+        buildingDef.isUtility = true;
+        buildingDef.DragBuild = true;
+        GeneratedBuildings.RegisterWithOverlay(OverlayModes.Logic.HighlightItemIDs, id);
+        return buildingDef;
+    }
+
+    public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
+    {
+        base.ConfigureBuildingTemplate(go, prefab_tag);
+        GeneratedBuildings.MakeBuildingAlwaysOperational(go);
+		BuildingConfigManager.Instance.IgnoreDefaultKComponent(typeof(RequiresFoundation), prefab_tag);
+		KAnimGraphTileVisualizer kAnimGraphTileVisualizer = go.AddOrGet<KAnimGraphTileVisualizer>();
+		kAnimGraphTileVisualizer.connectionSource = KAnimGraphTileVisualizer.ConnectionSource.Logic;
+		kAnimGraphTileVisualizer.isPhysicalBuilding = true;
+    }
+
+    public override void DoPostConfigureUnderConstruction(GameObject go)
+    {
+        base.DoPostConfigureUnderConstruction(go);
+        Constructable component = go.GetComponent<Constructable>();
+        component.isDiggingRequired = false;
+        component.choreTags = GameTags.ChoreTypes.WiringChores;
+        KAnimGraphTileVisualizer kAnimGraphTileVisualizer = go.AddOrGet<KAnimGraphTileVisualizer>();
+        kAnimGraphTileVisualizer.connectionSource = KAnimGraphTileVisualizer.ConnectionSource.Logic;
+        kAnimGraphTileVisualizer.isPhysicalBuilding = false;
+    }
+
+
+    public override void DoPostConfigureComplete(GameObject go)
+    {
+        go.AddOrGet<LogicWire>();
+        base.DoPostConfigureComplete(Wire.WattageRating.Max1000, go);
+    }
+}
---
+++ Source/MultiWiresMod/MultiWiresMod.cs
@@ -0,0 +1,49 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiWiresMod
+{
+
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    internal class MultiWiresMod_GeneratedBuildings_LoadGeneratedBuildings
+    {
+        private static void Prefix()
+        {
+            Debug.Log(" === MultiWiresMod_GeneratedBuildings_LoadGeneratedBuildings Prefix === " + MultiWireConfig.ID);
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.MULTIWIRE.NAME", "Multi Wire");
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.MULTIWIRE.DESC", "");
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.MULTIWIRE.EFFECT", "");
+
+            List<string> ls = new List<string>((string[])TUNING.BUILDINGS.PLANORDER[10].data);
+            ls.Add(MultiWireConfig.ID);
+            TUNING.BUILDINGS.PLANORDER[10].data = (string[])ls.ToArray();
+
+            TUNING.BUILDINGS.COMPONENT_DESCRIPTION_ORDER.Add(MultiWireConfig.ID);
+
+
+        }
+        private static void Postfix()
+        {
+
+            Debug.Log(" === MultiWiresMod_GeneratedBuildings_LoadGeneratedBuildings Postfix === " + MultiWireConfig.ID);
+            object obj = Activator.CreateInstance(typeof(MultiWireConfig));
+            BuildingConfigManager.Instance.RegisterBuilding(obj as IBuildingConfig);
+        }
+    }
+
+    [HarmonyPatch(typeof(Db), "Initialize")]
+    internal class MultiWiresMod_Db_Initialize
+    {
+        private static void Prefix(Db __instance)
+        {
+            Debug.Log(" === MultiWiresMod_Db_Initialize loaded === " + MultiWireConfig.ID);
+            List<string> ls = new List<string>((string[])Database.Techs.TECH_GROUPING["SmartStorage"]);
+            ls.Add(MultiWireConfig.ID);
+            Database.Techs.TECH_GROUPING["SmartStorage"] = (string[])ls.ToArray();
+        }
+    }
+    
+}
