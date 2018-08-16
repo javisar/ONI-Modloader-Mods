18208e40b03a2b68156710b215f97646a6c16e0e
javisar <javisar@gmail.com>
2018-08-16 09:59:41 +0200

    Updated InsulatedDoorsMod
    Still not working

--- Source/InsulatedDoorsMod/InsulatedPressureDoorConfig.cs
+++ Source/InsulatedDoorsMod/InsulatedPressureDoorConfig.cs
@@ -23,22 +23,22 @@ public class InsulatedPressureDoorConfig : IBuildingConfig
         BuildLocationRule build_location_rule = BuildLocationRule.Tile;
         EffectorValues nONE = NOISE_POLLUTION.NONE;
         BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(id, width, height, anim, hitpoints, construction_time, tIER, aLL_METALS, melting_point, build_location_rule, TUNING.BUILDINGS.DECOR.PENALTY.TIER1, nONE, 0.2f);
+		buildingDef.Overheatable = false;
+		buildingDef.RequiresPowerInput = true;
+		buildingDef.EnergyConsumptionWhenActive = 120f;
+		buildingDef.Entombable = false;
+		buildingDef.IsFoundation = true;
+		buildingDef.ViewMode = SimViewMode.PowerMap;
+		buildingDef.TileLayer = ObjectLayer.FoundationTile;
+		buildingDef.AudioCategory = "Metal";
+		buildingDef.PermittedRotations = PermittedRotations.R90;
+		buildingDef.SceneLayer = Grid.SceneLayer.TileMain;
+
+
 		//buildingDef.Insulation = 0.01f;
 		buildingDef.ThermalConductivity = 0.01f;		
-		buildingDef.Overheatable = false;
-        buildingDef.RequiresPowerInput = true;
         //buildingDef.UseStructureTemperature = false;
-        buildingDef.EnergyConsumptionWhenActive = 120f;
-        buildingDef.Entombable = false;
-        buildingDef.IsFoundation = true;
-        buildingDef.ViewMode = SimViewMode.PowerMap;
-        buildingDef.TileLayer = ObjectLayer.FoundationTile;
-        buildingDef.MaterialCategory = MATERIALS.ANY_BUILDABLE;
-        buildingDef.AudioCategory = "Metal";
-        buildingDef.PermittedRotations = PermittedRotations.R90;
-        buildingDef.SceneLayer = Grid.SceneLayer.TileMain;
-        SoundEventVolumeCache.instance.AddVolume("door_external_kanim", "Open_DoorPressure", NOISE_POLLUTION.NOISY.TIER2);
-        SoundEventVolumeCache.instance.AddVolume("door_external_kanim", "Close_DoorPressure", NOISE_POLLUTION.NOISY.TIER2);
+
         return buildingDef;
     }
 
@@ -54,20 +54,25 @@ public class InsulatedPressureDoorConfig : IBuildingConfig
 
     public override void DoPostConfigureComplete(GameObject go)
     {
-        Door door = go.UpdateComponentRequirement<Door>(true);
-        door.hasComplexUserControls = true;
-        door.unpoweredAnimSpeed = 1f;
-        go.UpdateComponentRequirement<AccessControl>(true);
-        go.UpdateComponentRequirement<KBoxCollider2D>(true);
-        Prioritizable.AddRef(go);
+		Door door = go.AddOrGet<Door>();
+		door.hasComplexUserControls = true;
+        door.unpoweredAnimSpeed = 0.65f;
+		door.poweredAnimSpeed = 5f;
+		door.doorClosingSoundEventName = "MechanizedAirlock_closing";
+		door.doorOpeningSoundEventName = "MechanizedAirlock_opening";
+		go.AddOrGet<AccessControl>();
+		go.AddOrGet<KBoxCollider2D>();
+		Prioritizable.AddRef(go);
         Workable workable = go.AddOrGet<Workable>();
         workable.workTime = 5f;
         GeneratedBuildings.RegisterLogicPorts(go, DoorConfig.INPUT_PORTS);
-        Object.DestroyImmediate(go.GetComponent<BuildingEnabledButton>());
-        BuildingTemplates.DoPostConfigure(go);
-        AccessControl component = go.GetComponent<AccessControl>();
-        component.controlEnabled = true;
-    }
+        Object.DestroyImmediate(go.GetComponent<BuildingEnabledButton>());        
+		BuildingTemplates.DoPostConfigure(go);
+		AccessControl component = go.GetComponent<AccessControl>();
+		component.controlEnabled = true;
+		KBatchedAnimController component2 = go.GetComponent<KBatchedAnimController>();
+		component2.initialAnim = "closed";
+	}
     
 	
     public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
