136d1d73df2f28eef6ad93f48e4c0518ae0ed45f
javisar <javisar@gmail.com>
2018-08-25 14:07:51 +0200

    Trying to fix IsAreaClear

--- Source/AllBuildingsDestroyableMod/AllBuildingsDestroyableMod.cs
+++ Source/AllBuildingsDestroyableMod/AllBuildingsDestroyableMod.cs
@@ -61,62 +61,131 @@ namespace AllBuildingsDestroyableMod
                 if ((UnityEngine.Object)gameObject != (UnityEngine.Object)null)
                 {
                     //Debug.Log(gameObject.PrefabID().ToString());
-                    /*
-                    if ((UnityEngine.Object)gameObject != (UnityEngine.Object)null)
-                    {
-                        if (!((UnityEngine.Object)gameObject.GetComponent<Wire>() == (UnityEngine.Object)null) && !((UnityEngine.Object)__instance.BuildingComplete.GetComponent<Wire>() == (UnityEngine.Object)null))
-                        {
-                            break;
-                        }
-                        fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
-                        flag = false;
-                        break;
-                    }
-                    if (tile_layer != ObjectLayer.NumLayers && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer] != (UnityEngine.Object)null && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer].GetComponent<BuildingPreview>() == (UnityEngine.Object)null)
-                    {
-                        fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
-                        flag = false;
-                        break;
-                    }
-                    */
-                    if ((UnityEngine.Object)gameObject != (UnityEngine.Object)null)
-                    {                                      
-                        switch (gameObject.PrefabID().ToString())
-                        {
-                            case "PropClock":
-                            case "PropDesk":
-                            case "PropElevator":
-                            case "PropFacilityChair":
-                            case "PropFacilityChairFlip":
-                            case "PropFacilityChandelier":
-                            case "PropFacilityCouch":
-                            case "PropFacilityDesk":
-                            case "PropFacilityDisplay":
-                            case "PropFacilityDisplay2":
-                            case "PropFacilityDisplay3":
-                            case "PropFacilityGlobeDroors":
-                            case "PropFacilityHangingLight":
-                            case "PropFacilityPainting":
-                            case "PropFacilityStatue":
-                            case "PropFacilityTable":
-                            case "PropFacilityWallDegree":
-                            case "PropLight":
-                            case "PropReceptionDesk":
-                            case "PropSkeleton":
-                            case "PropSurfaceSatellite1":
-                            case "PropSurfaceSatellite2":
-                            case "PropSurfaceSatellite3":
-                            case "PropTable":
-                            case "PropTallPlant":
-                                __result = true;
-                                fail_reason = null;
-                                break;
-                            default:                                
-                                break;
-                        }
-                    }
 
+                    if (!((UnityEngine.Object)gameObject.GetComponent<Wire>() == (UnityEngine.Object)null) && !((UnityEngine.Object)__instance.BuildingComplete.GetComponent<Wire>() == (UnityEngine.Object)null))
+                    {
+                        break;
+                    }
+                    //Debug.Log("Flag 1");
+                    bool flag2 = false;
+                    switch (gameObject.PrefabID().ToString())
+                    {
+                        case "PropClock":
+                        case "PropDesk":
+                        case "PropElevator":
+                        case "PropFacilityChair":
+                        case "PropFacilityChairFlip":
+                        case "PropFacilityChandelier":
+                        case "PropFacilityCouch":
+                        case "PropFacilityDesk":
+                        case "PropFacilityDisplay":
+                        case "PropFacilityDisplay2":
+                        case "PropFacilityDisplay3":
+                        case "PropFacilityGlobeDroors":
+                        case "PropFacilityHangingLight":
+                        case "PropFacilityPainting":
+                        case "PropFacilityStatue":
+                        case "PropFacilityTable":
+                        case "PropFacilityWallDegree":
+                        case "PropLight":
+                        case "PropReceptionDesk":
+                        case "PropSkeleton":
+                        case "PropSurfaceSatellite1":
+                        case "PropSurfaceSatellite2":
+                        case "PropSurfaceSatellite3":
+                        case "PropTable":
+                        case "PropTallPlant":
+                            //__result = true;
+                            //fail_reason = null;
+                            flag2 = true;
+                            break;
+                        default:
+                            //fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
+                            //__result = false;
+                            flag = false;
+                            break;
+                    }
+                    //fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
+                    //flag = false;
+                    if (!flag2) break;
                 }
+                if (tile_layer != ObjectLayer.NumLayers && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer] != (UnityEngine.Object)null && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer].GetComponent<BuildingPreview>() == (UnityEngine.Object)null)
+                {
+                    //fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
+                    //Debug.Log("Flag 2");
+                    flag = false;
+                    break;
+                }
+                /*
+                if ((UnityEngine.Object)gameObject != (UnityEngine.Object)null)
+                {
+                    if (!((UnityEngine.Object)gameObject.GetComponent<Wire>() == (UnityEngine.Object)null) && !((UnityEngine.Object)__instance.BuildingComplete.GetComponent<Wire>() == (UnityEngine.Object)null))
+                    {
+                        break;
+                    }
+                    fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
+                    flag = false;
+                    break;
+                }
+                if (tile_layer != ObjectLayer.NumLayers && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer] != (UnityEngine.Object)null && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer].GetComponent<BuildingPreview>() == (UnityEngine.Object)null)
+                {
+                    fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
+                    flag = false;
+                    break;
+                }
+                */
+                /*
+                if ((UnityEngine.Object)gameObject != (UnityEngine.Object)null)
+                {
+                    Debug.Log(gameObject.PrefabID().ToString());
+
+                    switch (gameObject.PrefabID().ToString())
+                    {
+                        case "PropClock":
+                        case "PropDesk":
+                        case "PropElevator":
+                        case "PropFacilityChair":
+                        case "PropFacilityChairFlip":
+                        case "PropFacilityChandelier":
+                        case "PropFacilityCouch":
+                        case "PropFacilityDesk":
+                        case "PropFacilityDisplay":
+                        case "PropFacilityDisplay2":
+                        case "PropFacilityDisplay3":
+                        case "PropFacilityGlobeDroors":
+                        case "PropFacilityHangingLight":
+                        case "PropFacilityPainting":
+                        case "PropFacilityStatue":
+                        case "PropFacilityTable":
+                        case "PropFacilityWallDegree":
+                        case "PropLight":
+                        case "PropReceptionDesk":
+                        case "PropSkeleton":
+                        case "PropSurfaceSatellite1":
+                        case "PropSurfaceSatellite2":
+                        case "PropSurfaceSatellite3":
+                        case "PropTable":
+                        case "PropTallPlant":
+                            //__result = true;
+                            //fail_reason = null;
+                            break;
+                        default:
+                            //fail_reason = UI.TOOLTIPS.HELP_BUILDLOCATION_OCCUPIED;
+                            //__result = false;
+                            flag = false;
+                            break;
+                    }
+                    if (!flag)
+                        break;
+                }
+                
+                if (tile_layer != ObjectLayer.NumLayers && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer] != (UnityEngine.Object)null && (UnityEngine.Object)Grid.Objects[num, (int)tile_layer].GetComponent<BuildingPreview>() == (UnityEngine.Object)null)
+                {
+                    flag = false;
+                    break;
+                }
+                */
+
                 /*
                 if ((UnityEngine.Object)gameObject != (UnityEngine.Object)null)
                 {
@@ -168,6 +237,15 @@ namespace AllBuildingsDestroyableMod
                 }
                 */
             }
+
+            //Debug.Log("__result: "+ __result+ ", fail_reason:" + fail_reason+", flag: "+flag);
+            
+            if (flag)
+            {
+                __result = true;
+                fail_reason = null;
+            }
+            
             //__result = flag && __instance.IsValidConduitLocation(source_go, cell, orientation, out fail_reason) && __instance.AreLogicPortsInValidPositions(source_go, cell, out fail_reason);            
         }
 
