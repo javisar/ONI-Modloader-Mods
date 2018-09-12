e385c9113aba26d57d620d905544bfe06a03eeea
javisar <javisar@gmail.com>
2018-09-12 17:50:52 +0200

    Functional Prototype BuldingModifierMod

--- Source/BuildingModifierMod/BuildingModifierState.cs
+++ Source/BuildingModifierMod/BuildingModifierState.cs
@@ -7,7 +7,9 @@ namespace BuildingModifierMod
     {
         public bool Enabled { get; set; } = true;
 
-        public Dictionary<string, Dictionary<string, object>> Modifiers { get; set; } = new Dictionary<string, Dictionary<string, object>>();
+		public bool Debug { get; set; } = false;
+
+		public Dictionary<string, Dictionary<string, object>> Modifiers { get; set; } = new Dictionary<string, Dictionary<string, object>>();
 
 
 		public static BaseStateManager<BuildingModifierState> StateManager
