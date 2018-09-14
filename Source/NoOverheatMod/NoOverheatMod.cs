e9b14ae11590246ab673df86bc92196aaf45ab1c
javisar <javisar@gmail.com>
2018-09-14 22:04:52 +0200

    Removed ConduitContentsBoiling/Frozen

--- Source/NoOverheatMod/NoOverheatMod.cs
+++ Source/NoOverheatMod/NoOverheatMod.cs
@@ -3,9 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace NoOverheatMod
 {
+    [HarmonyPatch(typeof(EventExtensions), "Trigger")]
+    internal class NoOverloadedWiresMod_EventExtensions_Trigger
+    {
+
+        private static bool Prefix(GameObject go, GameHashes hash, object data)
+        {
+            //Debug.Log(" === NoOverloadedWiresMod_EventExtensions_Trigger Prefix === ");
+            switch (hash) {
+                case GameHashes.ConduitContentsBoiling:
+                case GameHashes.ConduitContentsFrozen:
+                //case GameHashes.DoBuildingDamage:
+                    return false;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+
+    
     [HarmonyPatch(typeof(StructureTemperatureComponents), "DoOverheat")]
     internal class NoOverloadedWiresMod_StructureTemperatureComponents_DoOverheat
     {
@@ -17,4 +38,29 @@ namespace NoOverheatMod
             return false;
         }
     }
+    /*
+    [HarmonyPatch(typeof(ConduitFlow), "FreezeConduitContents")]
+    internal class NoOverloadedWiresMod_ConduitFlow_FreezeConduitContents
+    {
+
+        private static bool Prefix(int conduit_idx)
+        {
+            //Debug.Log(" === NoOverloadedWiresMod_ConduitFlow_FreezeConduitContents Prefix === ");
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ConduitFlow), "MeltConduitContents")]
+    internal class NoOverloadedWiresMod_ConduitFlow_MeltConduitContents
+    {
+
+        private static bool Prefix(int conduit_idx)
+        {
+            //Debug.Log(" === NoOverloadedWiresMod_ConduitFlow_FreezeConduitContents Prefix === ");
+
+            return false;
+        }
+    }
+    */
 }
