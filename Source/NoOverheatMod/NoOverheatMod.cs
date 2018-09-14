c2cbb06d9968d87df026224e5d39be8a1112f4f1
javisar <javisar@gmail.com>
2018-09-14 19:14:44 +0200

    NoOverheatMod

---
+++ Source/NoOverheatMod/NoOverheatMod.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoOverheatMod
+{
+    [HarmonyPatch(typeof(StructureTemperatureComponents), "DoOverheat")]
+    internal class NoOverloadedWiresMod_StructureTemperatureComponents_DoOverheat
+    {
+
+        private static bool Prefix(int sim_handle)
+        {
+            //Debug.Log(" === NoOverloadedWiresMod_StructureTemperatureComponents_DoOverheat Prefix === ");
+
+            return false;
+        }
+    }
+}
