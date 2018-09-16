458db2562e569379eebaae4ab1002ff5e17be271
javisar <javisar@gmail.com>
2018-09-16 14:42:13 +0200

    WorldGen Test

---
+++ Source/WorldGenReloadedMod/WorldGenReloadedMod.cs
@@ -0,0 +1,69 @@
+﻿using Harmony;
+using Klei;
+using ProcGen;
+using ProcGen.Noise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace WorlGenReloadedMod
+{
+    [HarmonyPatch(typeof(ProcGen.World), "LoadZones")]
+    internal class WorlGenReloadedMod_World_LoadZones
+    {
+        //private static FieldInfo ZonesF = AccessTools.Field(typeof(ProcGen.World), "Zones");
+        //private static FieldInfo ZoneFilesF = AccessTools.Field(typeof(ProcGen.World), "ZoneFiles");
+        private static  FieldInfo ZoneLookupTableF = AccessTools.Field(typeof(ProcGen.World), "ZoneLookupTable");
+
+        private static bool Prefix(ProcGen.World __instance, NoiseTreeFiles noise, string path) {
+            Debug.Log(" === WorlGenReloadedMod_World_LoadZones Prefix === ");
+            Debug.Log(path);
+
+            foreach (WeightedName zoneFile in __instance.ZoneFiles)
+            {
+                SubWorld subWorld = null;
+                string text = WorldGenSettings.GetSimpleName(zoneFile.name);
+                Debug.Log(text);
+                //if (text != "HotMarsh") return true;
+
+
+                if (zoneFile.overrideName != null && zoneFile.overrideName.Length > 0)
+                {
+                    text = zoneFile.overrideName;
+                }
+                //if (!ZoneLookupTable.ContainsKey(text))
+                if (!((Dictionary < string, SubWorld > )ZoneLookupTableF.GetValue(__instance)).ContainsKey(text))
+                {
+                    SubWorldFile subWorldFile = YamlIO<SubWorldFile>.LoadFile(path + zoneFile.name + ".yaml");
+                    if (subWorldFile != null)
+                    {
+                        subWorld = subWorldFile.zone;
+                        //
+                        subWorld.featureTemplates["feature_geyser_generic"] = 10;
+                        //
+                        subWorld.name = text;
+                        subWorld.pdWeight = zoneFile.weight;
+                        //ZoneLookupTable[text] = subWorld;
+                        ((Dictionary<string, SubWorld>)ZoneLookupTableF.GetValue(__instance))[text] = subWorld;
+                        noise.LoadTree(subWorld.biomeNoise, path);
+                        noise.LoadTree(subWorld.densityNoise, path);
+                        noise.LoadTree(subWorld.overrideNoise, path);
+                    }
+                    else
+                    {
+                        Debug.LogWarning("WorldGen: Attempting to load zone: " + zoneFile.name + " failed");
+                    }
+                }
+                else
+                {
+                    subWorld = ((Dictionary<string, SubWorld>)ZoneLookupTableF.GetValue(__instance))[text];
+                }
+                __instance.Zones[text] = subWorld;
+            }
+            return false;
+        }
+
+    }
+}
