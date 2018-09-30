8514af27004c621779609c13b484bb600b8550ec
javisar <javisar@gmail.com>
2018-09-30 16:46:40 +0200

    Moved howto's to a new folder

---
+++ Source/Patches/AutoSweeperThroughWallsMod.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoSweeperThroughWallsMod
+{
+    [HarmonyPatch(typeof(Grid), "IsPhysicallyAccessible", null)]
+    internal class AutoSweeperThroughWallsMod_Grid_IsPhysicallyAccessible
+    {
+        private static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
