8aa49b7aea05d5bb07d366665f9a0fa70991eb32
Szymon Rudziński <swisrud@gmail.com>
2018-12-08 05:28:40 +0100

    Revert "Make check not case sensitive"
    This reverts commit 8ba4cf3bd2e9bde573667af19c7c40deb5797419.

--- Source/MaterialColor/TextFilter.cs
+++ Source/MaterialColor/TextFilter.cs
@@ -32,23 +32,18 @@ namespace MaterialColor
         /// <summary>
         /// Checks if value passes through the filter's ruleset.
         /// </summary>
-        public readonly Func<string, bool> Check;
+        public Func<string, bool> Check;
 
         private readonly List<string> Rules;
 
         private bool InclusiveCheck(string value)
         {
-            return this.ContainsAny(value);
+            return this.Rules.Any(rule => value.Contains(rule));
         }
 
         private bool ExclusiveCheck(string value)
         {
-            return !this.ContainsAny(value);
-        }
-
-        private bool ContainsAny(string value)
-        {
-            return this.Rules.Any(rule => value.ToUpperInvariant().Contains(rule.ToUpperInvariant()));
+            return !this.Rules.Any(rule => value.Contains(rule));
         }
     }
 }
