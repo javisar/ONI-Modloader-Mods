1ef43f99e9d0447fe6aead0444613806e406b758
javisar <javisar@gmail.com>
2018-12-28 11:27:52 +0100

    Morefood mod prototype

--- Source/MoreFood/MoreFoodMod.cs
+++ Source/MoreFood/MoreFoodMod.cs
@@ -18,8 +18,13 @@ namespace MoreFood
 			Debug.Log(" === MoreFood_CookingStationConfig_ConfigureRecipes Postfix === ");
 
 			MeatloafConfig.recipe = CreateMeatloafRecipe();
+			Strings.Add("STRINGS.BUILDINGS.PREFABS.MEATLOAF.NAME", "Meatloaf");
 
 			FungiSaladConfig.recipe = CreateFungiSaladRecipe();
+			Strings.Add("STRINGS.BUILDINGS.PREFABS.FUNGISALAD.NAME", "Fungi Salad");
+
+
+
 		}
 
 		private static ComplexRecipe CreateMeatloafRecipe()
@@ -27,7 +32,7 @@ namespace MoreFood
 
 			ComplexRecipe.RecipeElement[] inputs = new ComplexRecipe.RecipeElement[2]
 			{
-				new ComplexRecipe.RecipeElement(BasicPlantFoodConfig.ID, 10f),
+				new ComplexRecipe.RecipeElement(BasicPlantFoodConfig.ID, 2f),
 				new ComplexRecipe.RecipeElement(SpiceNutConfig.ID, 1f)
 			};
 			ComplexRecipe.RecipeElement[] outputs = new ComplexRecipe.RecipeElement[1]
@@ -54,8 +59,8 @@ namespace MoreFood
 
 			ComplexRecipe.RecipeElement[] inputs = new ComplexRecipe.RecipeElement[2]
 			{
-				new ComplexRecipe.RecipeElement(BasicPlantFoodConfig.ID, 10f),
-				new ComplexRecipe.RecipeElement(MushroomConfig.ID, 2f)
+				new ComplexRecipe.RecipeElement(BasicPlantFoodConfig.ID, 2f),
+				new ComplexRecipe.RecipeElement(MushroomConfig.ID, 1f)
 			};
 			ComplexRecipe.RecipeElement[] outputs = new ComplexRecipe.RecipeElement[1]
 			{
