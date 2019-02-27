a3db6e7e9da34870fa4c47c7c3f0fc307dd6545b
javisar <javisar@gmail.com>
2019-02-27 11:19:11 +0100

    Added more speeds

--- Source/SpeedControlMod/SpeedControlMod.cs
+++ Source/SpeedControlMod/SpeedControlMod.cs
@@ -2,15 +2,57 @@
 {
     using Harmony;
     using System;
-    using UnityEngine;
+	using System.Reflection;
+	using UnityEngine;
     using Debug = Debug;
 
-    [HarmonyPatch(typeof(SpeedControlScreen), "OnChanged", new Type[0])]
-    internal static class SpeedControlMod
-    {
+	[HarmonyPatch(typeof(SpeedControlScreen), "SetSpeed")]
+	internal static class SpeedControlMod_SpeedControlScreen_SetSpeed
+	{
+		private static FieldInfo speedF = AccessTools.Field(typeof(SpeedControlScreen), "speed");
+		private static FieldInfo slowButtonF = AccessTools.Field(typeof(SpeedControlScreen), "slowButton");
+		private static FieldInfo mediumButtonF = AccessTools.Field(typeof(SpeedControlScreen), "mediumButton");
+		private static FieldInfo fastButtonF = AccessTools.Field(typeof(SpeedControlScreen), "fastButton");
+
+		private static bool Prefix(SpeedControlScreen __instance, int Speed)
+		{
+			Debug.Log(" === SpeedControlMod_SpeedControlScreen_SetSpeed INI === Speed = " + Speed);
+
+			speedF.SetValue(__instance, Speed % 9);
+			switch ((int) speedF.GetValue(__instance))
+			{
+				case 0:
+					((KToggle)slowButtonF.GetValue(__instance)).Select();
+					((KToggle)slowButtonF.GetValue(__instance)).isOn = true;
+					((KToggle)mediumButtonF.GetValue(__instance)).isOn = false;
+					((KToggle)fastButtonF.GetValue(__instance)).isOn = false;					
+					break;
+				case 1:
+					((KToggle)mediumButtonF.GetValue(__instance)).Select();
+					((KToggle)slowButtonF.GetValue(__instance)).isOn = false;
+					((KToggle)mediumButtonF.GetValue(__instance)).isOn = true;
+					((KToggle)fastButtonF.GetValue(__instance)).isOn = false;
+					break;
+				case 2:
+					((KToggle)fastButtonF.GetValue(__instance)).Select();
+					((KToggle)slowButtonF.GetValue(__instance)).isOn = false;
+					((KToggle)mediumButtonF.GetValue(__instance)).isOn = false;
+					((KToggle)fastButtonF.GetValue(__instance)).isOn = true;
+					break;
+				default:
+					break;
+			}
+			__instance.OnSpeedChange();
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(SpeedControlScreen), "OnChanged", new Type[0])]
+    internal static class SpeedControlMod_SpeedControlScreen_OnChanged
+	{
         private static bool Prefix(SpeedControlScreen __instance)
         {
-            //Debug.Log(" === SpeedControlMod INI === ");
+            Debug.Log(" === SpeedControlMod_SpeedControlScreen_OnChanged INI === __instance.GetSpeed() = " + __instance.GetSpeed());
 
             if (__instance.IsPaused)
             {
@@ -18,23 +60,47 @@
             }
             else if (__instance.GetSpeed() == 0)
             {
-                Time.timeScale = __instance.normalSpeed;
+                Time.timeScale = 0.06f;
             }
             else if (__instance.GetSpeed() == 1)
             {
-				//Time.timeScale = __instance.fastSpeed;
-				Time.timeScale = __instance.ultraSpeed;
+				Time.timeScale = 0.5f;
 			}
             else if (__instance.GetSpeed() == 2)
             {
-                Time.timeScale = 10f;
+                Time.timeScale = 1f;
             }
+			else if (__instance.GetSpeed() == 3)
+			{
+				Time.timeScale = 2f;
+			}
+			else if (__instance.GetSpeed() == 4)
+			{
+				Time.timeScale = 3f;
+			}
+			else if (__instance.GetSpeed() == 5)
+			{
+				Time.timeScale = 5f;
+			}
+			else if (__instance.GetSpeed() == 6)
+			{
+				Time.timeScale = 10f;
+			}
+			else if (__instance.GetSpeed() == 7)
+			{
+				Time.timeScale = 15f;
+			}
+			else if (__instance.GetSpeed() == 8)
+			{
+				Time.timeScale = 30f;
+			}
 
-            //__instance.OnGameSpeedChanged?.Invoke();
+			//__instance.OnGameSpeedChanged?.Invoke();
 
-            //Debug.Log(" === SpeedControlMod END === ");
+			//Debug.Log(" === SpeedControlMod END === ");
+			Debug.Log(" === SpeedControlMod_SpeedControlScreen_OnChanged INI === Time.timeScale = " + Time.timeScale);
 
-            return false;
+			return false;
         }
     }
 }
