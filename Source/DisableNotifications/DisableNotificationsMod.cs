d6d6c0a48d7f3c838d74eba027f576bc9af7787f
javisar <javisar@gmail.com>
2019-03-08 13:42:12 +0100

    DisableNotifications mod first prototype

---
+++ Source/DisableNotifications/DisableNotificationsMod.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using UnityEngine;
+using static Notification;
+
+namespace DisableNotifications
+{
+	[HarmonyPatch(typeof(StatusItem), "AddNotification")]
+	internal class DisableNotificationsMod_StatusItem_AddNotification
+	{
+
+		private static void Postfix(StatusItem __instance)
+		{
+			if (!DisableNotificationsState.StateManager.State.Enabled) return;
+
+			Debug.Log(" === DisableNotificationsMod_StatusItem_AddNotification Postfix === ");
+
+			//__instance.shouldNotify = false;
+			
+			//__instance.notificationDelay = 10f;
+
+			/*
+			BuildingStatusItems.CreateStatusItems() 
+			CreatureStatusItems.CreateStatusItems() 
+			DuplicantStatusItems.CreateStatusItems() 
+			*/
+		}
+	}
+
+	[HarmonyPatch(typeof(Notification), MethodType.Constructor, new Type[] { typeof(string), typeof(NotificationType), typeof(HashedString), typeof(Func<List<Notification>, object, string>), typeof(object), typeof(bool), typeof(float), typeof(ClickCallback), typeof(object), typeof(Transform) })]
+	internal class DisableNotificationsMod_Notification_Constructor
+	{
+
+		private static void Postfix(ref Notification __instance)
+		{
+			if (!DisableNotificationsState.StateManager.State.Enabled) return;
+
+			Debug.Log(" === DisableNotificationsMod_Notification_Constructor Postfix === ");
+			__instance.expires = true;
+		}
+	}
+
+	[HarmonyPatch(typeof(NotificationScreen), "Update")]
+	internal class DisableNotificationsMod_Notification_Update
+	{
+
+		private static void Prefix(NotificationScreen __instance)
+		{
+			if (!DisableNotificationsState.StateManager.State.Enabled) return;
+
+			Debug.Log(" === DisableNotificationsMod_Notification_Update Prefix === "+ __instance.lifetime);			
+		}
+	}
+}
---
+++ Source/DisableNotifications/DisableNotificationsState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ONI_Common.Json;
+
+namespace DisableNotifications
+{
+	public class DisableNotificationsState
+	{
+		public bool Enabled { get; set; } = true;
+
+		public int Parameter { get; set; } = 512;
+
+
+		public static BaseStateManager<DisableNotificationsState> StateManager
+			= new BaseStateManager<DisableNotificationsState>("DisableNotifications");
+	}
+}
