e8931884a29660e4271ca13c489fa59791e85f10
javisar <javisar@gmail.com>
2019-05-01 11:25:10 +0200

    Added config file to SpeedControl

--- Source/SpeedControl/SpeedControl.cs
+++ Source/SpeedControl/SpeedControl.cs
@@ -1,16 +1,30 @@
-﻿namespace SpeedControl
-{
-    using Harmony;
-    using System;
-    using UnityEngine;
-    using Debug = Debug;
+﻿using Harmony;
+using UnityEngine;
 
-    [HarmonyPatch(typeof(SpeedControlScreen), "OnChanged", new Type[0])]
+
+namespace SpeedControl
+{
+    
+
+    internal class SpeedControlMod_OnLoad
+    {
+        public static void OnLoad(string modPath)
+        {
+            SpeedControlConfig.LoadConfig(modPath);
+        }
+    }
+
+
+    [HarmonyPatch(typeof(SpeedControlScreen), "OnChanged")]
     internal static class SpeedControl
     {
         private static bool Prefix(SpeedControlScreen __instance)
         {
-            //Debug.Log(" === SpeedControl INI === "+ __instance.ultraSpeed);
+            if (!SpeedControlConfig.Config.Enabled)
+                return true;
+
+            if (SpeedControlConfig.Config.Logging)
+                Debug.Log(" === SpeedControl INI === "+ Time.timeScale);
 
             if (__instance.IsPaused)
             {
@@ -18,21 +32,22 @@
             }
             else if (__instance.GetSpeed() == 0)
             {
-                Time.timeScale = 1f;
+                Time.timeScale = SpeedControlConfig.Config.SpeedMultiplier1;
             }
             else if (__instance.GetSpeed() == 1)
             {
 				//Time.timeScale = __instance.fastSpeed;
-				Time.timeScale = 3f;
-			}
+				Time.timeScale = SpeedControlConfig.Config.SpeedMultiplier2;
+            }
             else if (__instance.GetSpeed() == 2)
             {
-                Time.timeScale = 10f;
+                Time.timeScale = SpeedControlConfig.Config.SpeedMultiplier3;
             }
 
             //__instance.OnGameSpeedChanged?.Invoke();
 
-            //Debug.Log(" === SpeedControl END === ");
+            if (SpeedControlConfig.Config.Logging)
+                Debug.Log(" === SpeedControl END === " + Time.timeScale);
 
             return false;
         }
---
+++ Source/SpeedControl/SpeedControlConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SpeedControl
+{
+    public class SpeedControlConfig
+    {
+        public bool Enabled { get; set; } = true;
+        public bool Logging { get; set; } = false;
+
+        public float SpeedMultiplier1 { get; set; } = 1f;
+        public float SpeedMultiplier2 { get; set; } = 3f;
+        public float SpeedMultiplier3 { get; set; } = 10f;
+
+
+
+        public static SpeedControlConfig Config = null;
+        public static void LoadConfig(string modPath) { Config = LoadConfig<SpeedControlConfig>(Path.Combine(modPath, "Config/SpeedControlConfig.json"));  }
+        
+
+        protected static T LoadConfig<T>(string path)
+        {
+            JsonSerializer serializer = JsonSerializer.CreateDefault(new JsonSerializerSettings { Formatting = Formatting.Indented, ObjectCreationHandling = ObjectCreationHandling.Replace });
+            T result;
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                using (JsonTextReader jsonReader = new JsonTextReader(streamReader))
+                {
+                    result = serializer.Deserialize<T>(jsonReader);
+                    jsonReader.Close();
+                }
+                streamReader.Close();
+            }
+            return result;            
+        }
+
+    }
+}
