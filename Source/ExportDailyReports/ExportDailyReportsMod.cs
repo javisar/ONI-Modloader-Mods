611ef43511a681a4b7db4a5b8d96e8d8a99b2ff0
javisar <javisar@gmail.com>
2019-05-06 16:38:46 +0200

    Export Daily Reports moved to Options Menu

--- Source/ExportDailyReports/ExportDailyReportsMod.cs
+++ Source/ExportDailyReports/ExportDailyReportsMod.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using Harmony;
 using UnityEngine;
 using static KButtonMenu;
@@ -10,46 +12,53 @@ using static ReportManager;
 
 namespace ExportDailyReports
 {
-    [HarmonyPatch(typeof(PauseScreen), "OnPrefabInit")]
-    internal class ExportDailyReports_PauseScreen_OnPrefabInit
-    {
-        private static FieldInfo buttonsF = AccessTools.Field(typeof(KButtonMenu), "buttons");
+    [HarmonyPatch(typeof(OptionsMenuScreen), "OnPrefabInit")]
+    internal class ExportDailyReports_OptionsMenuScreen_OnPrefabInit
+	{
+		
+
+		private static FieldInfo buttonsF = AccessTools.Field(typeof(KButtonMenu), "buttons");
         private static void Postfix(PauseScreen __instance)
         {
-            Debug.Log(" === ExportDailyReports_PauseScreen_OnPrefabInit Postfix === ");
-            /*
-            IList<ButtonInfo> buttons = (IList<ButtonInfo>) buttonsF.GetValue(__instance);
-            buttons.Insert(
-                    buttons.Count-2,
-                    new ButtonInfo("Export Daily Reports", Action.NumActions, OnExportDailyReports)
-                );
-            */
-            List<ButtonInfo> ls1 = new List<ButtonInfo>((ButtonInfo[])(IList<ButtonInfo>)buttonsF.GetValue(__instance));
-            ls1.Insert(ls1.Count - 2, new ButtonInfo("Export Daily Reports", Action.NumActions, OnExportDailyReports));
-            buttonsF.SetValue(__instance, ls1);
-        }
+            Debug.Log(" === ExportDailyReports_OptionsMenuScreen_OnPrefabInit Postfix === ");           
+
+			var buttonList = new List<ButtonInfo>((IList<ButtonInfo>)buttonsF.GetValue(__instance)) {
+				new ButtonInfo("Export Daily Reports", Action.NumActions, OnExportDailyReports)
+			};
+			buttonsF.SetValue(__instance, buttonList);
+		}
 
         private static void OnExportDailyReports()
         {
-            //ConfirmDecision(UI.FRONTEND.MAINMENU.DESKTOPQUITCONFIRM, OnDesktopQuitConfirm);
-            List<DailyReport> dailyReports = ReportManager.Instance.reports;
+			List<DailyReport> dailyReports = ReportManager.Instance.reports;
 
-            foreach (var report in dailyReports)
+			//string AssemblyPath = Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path));
+			string reportsPath = System.Reflection.Assembly.GetAssembly(typeof(ExportDailyReports_OptionsMenuScreen_OnPrefabInit)).Location;
+			string reportsFile = Path.Combine(Path.GetDirectoryName(SaveLoader.GetActiveSaveFilePath()),Path.GetFileNameWithoutExtension(SaveLoader.GetActiveSaveFilePath()) + "_" + dailyReports.Count + ".csv");
+
+			StringBuilder csvFields = new StringBuilder("");
+			StringBuilder csvData = new StringBuilder("");
+
+			csvFields.Append("Cycle");
+
+			int idx = 0;
+			foreach (var report in dailyReports)
             {
                 try
                 {
-                    Debug.Log(report.day);
-                    /*
+					//Debug.Log(report.day);
+					csvData.Append(report.day);
+					/*
                     foreach (var entry in report.reportEntries)
                     {
                         Debug.Log(entry);
                     }
                     */
-
-                    int num = 1;
+					
+					int num = 1;
                     foreach (KeyValuePair<ReportManager.ReportType, ReportManager.ReportGroup> reportGroup in ReportManager.Instance.ReportGroups)
                     {
-                        ReportManager.ReportEntry entry = report.GetEntry(reportGroup.Key);
+						ReportManager.ReportEntry entry = report.GetEntry(reportGroup.Key);
                         int num2 = num;
                         ReportManager.ReportGroup value = reportGroup.Value;
                         if (num2 != value.group)
@@ -72,25 +81,45 @@ namespace ExportDailyReports
                         ReportManager.ReportGroup value4 = reportGroup.Value;
                         if (value4.isHeader)
                         {
-                            CreateHeader(reportGroup.Value);
+                            //CreateHeader(reportGroup.Value);
                         }
                         else if (flag2)
                         {
-                            CreateOrUpdateLine(entry, reportGroup.Value, flag2);
+                            CreateOrUpdateLine(entry, reportGroup.Value, flag2, idx, csvFields, csvData);
                         }
                     }
-                }
+
+					csvData.Append("\n");
+				}
                 catch (Exception e)
                 {
                     Debug.LogError(e);
                 }
+				idx++;
             }
+
+			try
+			{
+				Debug.Log("Exporting Daily Reports: "+reportsFile);
+				File.WriteAllText(reportsFile, csvFields.ToString()+"\n"+ csvData.ToString());
+
+				InfoDialogScreen infoDialogScreen = (InfoDialogScreen)GameScreenManager.Instance.StartScreen(ScreenPrefabs.Instance.InfoDialogScreen.gameObject, GameScreenManager.Instance.ssOverlayCanvas.gameObject);				
+				infoDialogScreen.SetHeader("Export Daily Reports").AddPlainText("Daily reports exported.");
+				infoDialogScreen.Show();
+			}
+			catch (Exception e)
+			{
+				Debug.LogError(e);
+				InfoDialogScreen infoDialogScreen = (InfoDialogScreen)GameScreenManager.Instance.StartScreen(ScreenPrefabs.Instance.InfoDialogScreen.gameObject, GameScreenManager.Instance.ssOverlayCanvas.gameObject);
+				infoDialogScreen.SetHeader("Export Daily Reports").AddPlainText("Error exporting daily reports:\n"+e.ToString());
+				infoDialogScreen.Show();
+			}
         }
 
 
         private static void CreateHeader(ReportManager.ReportGroup reportGroup)
         {
-            Debug.Log(reportGroup.stringKey);
+            Debug.Log("header: "+reportGroup.stringKey);
             /*
             GameObject value = null;
             lineItems.TryGetValue(reportGroup.stringKey, out value);
@@ -107,9 +136,10 @@ namespace ExportDailyReports
             */
         }
 
-        private static void CreateOrUpdateLine(ReportManager.ReportEntry entry, ReportManager.ReportGroup reportGroup, bool is_line_active)
+        private static void CreateOrUpdateLine(ReportManager.ReportEntry entry, ReportManager.ReportGroup reportGroup, bool is_line_active, int idx, StringBuilder csvFields, StringBuilder csvData)
         {
-            /*
+			//Debug.Log("line: " + reportGroup.stringKey);
+			/*
             GameObject value = null;
             lineItems.TryGetValue(reportGroup.stringKey, out value);
             if (!is_line_active)
@@ -133,22 +163,23 @@ namespace ExportDailyReports
             }
             return value;
             */
-            SetMainEntry(entry, reportGroup);
+			SetMainEntry(entry, reportGroup, idx, csvFields, csvData);
         }
 
 
-        public static void SetMainEntry(ReportManager.ReportEntry entry, ReportManager.ReportGroup reportGroup)
+        public static void SetMainEntry(ReportManager.ReportEntry entry, ReportManager.ReportGroup reportGroup, int idx, StringBuilder csvFields, StringBuilder csvData)
         {
-            SetLine(entry, reportGroup);
+            SetLine(entry, reportGroup, idx, csvFields, csvData);
             addedValue = float.NegativeInfinity;
             removedValue = float.NegativeInfinity;
             netValue = float.NegativeInfinity;
-
+			/*
             int currentContextCount = entry.contextEntries.Count;
             for (int i = 0; i < entry.contextEntries.Count; i++)
             {               
-                //SetLine(entry.contextEntries[i], reportGroup);
+                SetLine(entry.contextEntries[i], reportGroup);
             }
+			*/
             /*
             if (mainRow == null)
             {
@@ -183,11 +214,11 @@ namespace ExportDailyReports
         private static float removedValue = float.NegativeInfinity;
         private static float netValue = float.NegativeInfinity;
 
-        public static void SetLine(ReportManager.ReportEntry entry, ReportManager.ReportGroup reportGroup)
+        public static void SetLine(ReportManager.ReportEntry entry, ReportManager.ReportGroup reportGroup, int idx, StringBuilder csvFields, StringBuilder csvData)
         {
-            //this.entry = entry;
-            //this.reportGroup = reportGroup;
-            /*
+			//this.entry = entry;
+			//this.reportGroup = reportGroup;
+			/*
             ListPool<ReportManager.ReportEntry.Note, ReportScreenEntryRow>.PooledList pos_notes = ListPool<ReportManager.ReportEntry.Note, ReportScreenEntryRow>.Allocate();
             entry.IterateNotes(delegate (ReportManager.ReportEntry.Note note)
             {
@@ -197,17 +228,6 @@ namespace ExportDailyReports
                     //Debug.Log("pos "+note.note+" = "+note.value);
                 }
             });
-            */
-            List<ReportManager.ReportEntry.Note> pos_notes = new List<ReportManager.ReportEntry.Note>();
-            entry.IterateNotes(delegate (ReportManager.ReportEntry.Note note)
-            {
-                if (note.value > 0f)
-                {
-                    pos_notes.Add(note);
-                    //Debug.Log("pos "+note.note+" = "+note.value);
-                }
-            });
-            /*
             ListPool<ReportManager.ReportEntry.Note, ReportScreenEntryRow>.PooledList neg_notes = ListPool<ReportManager.ReportEntry.Note, ReportScreenEntryRow>.Allocate();
             entry.IterateNotes(delegate (ReportManager.ReportEntry.Note note)
             {
@@ -218,7 +238,18 @@ namespace ExportDailyReports
                 }
             });
             */
-            List<ReportManager.ReportEntry.Note> neg_notes = new List<ReportManager.ReportEntry.Note>();
+
+			List<ReportManager.ReportEntry.Note> pos_notes = new List<ReportManager.ReportEntry.Note>();
+			entry.IterateNotes(delegate (ReportManager.ReportEntry.Note note)
+			{
+				if (note.value > 0f)
+				{
+					pos_notes.Add(note);
+					//Debug.Log("pos "+note.note+" = "+note.value);
+				}
+			});
+
+			List<ReportManager.ReportEntry.Note> neg_notes = new List<ReportManager.ReportEntry.Note>();
             entry.IterateNotes(delegate (ReportManager.ReportEntry.Note note)
             {
                 if (note.value < 0f)
@@ -227,7 +258,9 @@ namespace ExportDailyReports
                     //Debug.Log("neg " + note.note + " = " + note.value);
                 }
             });
-            //LayoutElement component = name.GetComponent<LayoutElement>();
+
+			//LayoutElement component = name.GetComponent<LayoutElement>();
+			/*
             if (entry.context == null)
             {                
                 Debug.Log(reportGroup.stringKey);
@@ -236,7 +269,12 @@ namespace ExportDailyReports
             {
                 Debug.Log(entry.context);                
             }
-            if (addedValue != entry.Positive)
+			*/
+
+			string columnName = (entry.context == null ? reportGroup.stringKey : entry.context);
+
+
+			if (addedValue != entry.Positive)
             {
                 string text = reportGroup.formatfn(entry.Positive);
                 if (reportGroup.groupFormatfn != null && entry.context == null)
@@ -247,10 +285,14 @@ namespace ExportDailyReports
                     text = reportGroup.groupFormatfn(entry.Positive, num6);
                 }
                 //added.text = text;
-                Debug.Log("Added: "+text+" = " + entry.Positive);
                 addedValue = entry.Positive;
-            }
-            if (removedValue != entry.Negative)
+				//Debug.Log("Added: " + text + " = " + addedValue);
+				if (idx == 1) csvFields.Append(","+ CleanHeader(columnName) + " (Added)");
+				else if (!csvFields.ToString().Contains(CleanHeader(columnName)))
+					Debug.Log("Not found: " + CleanHeader(columnName));
+				csvData.Append(","+ addedValue);
+			}
+			if (removedValue != entry.Negative)
             {
                 string text2 = reportGroup.formatfn(entry.Negative);
                 if (reportGroup.groupFormatfn != null && entry.context == null)
@@ -261,9 +303,13 @@ namespace ExportDailyReports
                     text2 = reportGroup.groupFormatfn(entry.Negative, num7);
                 }
                 //removed.text = text2;
-                Debug.Log("Removed: " + text2 + " = " + entry.Negative);
                 removedValue = entry.Negative;
-            }
+				//Debug.Log("Removed: " + text2 + " = " + removedValue);
+				if (idx == 1) csvFields.Append("," + CleanHeader(columnName) + " (Removed)");
+				else if (!csvFields.ToString().Contains(CleanHeader(columnName)))
+					Debug.Log("Not found: " + CleanHeader(columnName));
+				csvData.Append("," + removedValue);
+			}
             if (netValue != entry.Net)
             {
                 string text3 = (reportGroup.formatfn != null) ? reportGroup.formatfn(entry.Net) : entry.Net.ToString();
@@ -274,13 +320,40 @@ namespace ExportDailyReports
                     num8 = Mathf.Max(num8, 1f);
                     text3 = reportGroup.groupFormatfn(entry.Net, num8);
                 }
-                //net.text = text3;
-                Debug.Log("Net: " + text3 + " = " + entry.Net);
+                //net.text = text3;                
                 netValue = entry.Net;
-            }
+				//Debug.Log("Removed: " + text3 + " = " + netValue);
+				if (idx == 1) csvFields.Append("," + CleanHeader(columnName) + " (Removed)");
+				else if (!csvFields.ToString().Contains(CleanHeader(columnName)))
+					Debug.Log("Not found: "+ CleanHeader(columnName));
+				csvData.Append("," + netValue);
+			}
             pos_notes.Clear();
             neg_notes.Clear();
         }
 
+
+		public static string CleanHeader(string txt)
+		{
+			/*
+			Regex pattern = new Regex(@"<link="".+ "">(?<txt1>.+)</link>(?<txt2>.+)");
+			Match match = pattern.Match(txt);
+			string outT = "";
+			if (match.Success)
+			{
+				outT = match.Groups["txt1"].Value+" "+ match.Groups["txt2"].Value;
+			}
+			else
+			{
+				outT = txt;
+			}
+			*/
+			string outT = txt.Replace(":", "");
+			int idx = outT.IndexOf("\">");
+			if (idx > 0)
+				outT = outT.Substring(idx + 2, outT.IndexOf("</link>") - idx - 2) + outT.Substring(outT.IndexOf("</link>")+7);
+
+			return outT.Trim();
+		}
     }
 }
