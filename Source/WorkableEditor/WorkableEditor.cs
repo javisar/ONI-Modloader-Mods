dc16fffbdcc3fb032fbf1f7f5cf849369413ea27
javisar <javisar@gmail.com>
2019-05-10 12:00:26 +0200

    FastMode renamed to WorkableEditor
    Added config file
    Refactored to allow multiples Workables

---
+++ Source/WorkableEditor/WorkableEditor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Harmony;
+
+namespace WorkableEditorMod
+{
+
+	[HarmonyPatch(typeof(Game), "OnPrefabInit")]
+	internal class WorkableEditorMod_Game_OnPrefabInit
+	{
+		private static void Postfix(Game __instance)
+		{
+			//Debug.Log(" === WorkableEditorMod_Game_OnPrefabInit Postfix === ");
+			if(!WorkableEditorConfig.Instance.Enabled) return;
+
+			if (WorkableEditorConfig.Instance.Logging)
+			{
+				Debug.Log(" === WorkableEditorMod_Game_OnPrefabInit === 'Workable' Derived Types:");
+				foreach (Type tp in WorkableEditorUtils.WorkableSubTypes)
+				{
+					Debug.Log(tp.FullName);
+				}
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(Workable), "GetEfficiencyMultiplier", new[] { typeof(Worker) })]
+    internal static class WorkableEditorMod_Workable_GetEfficiencyMultiplier
+	{
+        private static void Postfix(Workable __instance, Worker worker, ref float __result)
+        {
+			if (!WorkableEditorConfig.Instance.Enabled) return;
+			//Debug.Log(" === WorkEditorMod_Constructable_GetEfficiencyMultiplier === " + __instance.GetType().ToString());
+
+			__result = WorkableEditorUtils.GetMultiplier("EfficiencyMultiplier", __result, __instance);			
+			
+			/*
+			
+			*/
+
+			/*
+			foreach (var workable in WorkableEditorConfig.Config.Workables)
+			{
+				//Debug.Log("workable.Key: "+ workable.Key);
+				Type type = AccessTools.TypeByName(workable.Key);
+				//Debug.Log("type: " + type);
+				if (WorkableEditorMod_Utils.WorkableSubTypes.Contains(type)) {
+					//Debug.Log("__instance.GetType(): "+ __instance.GetType());
+					if (__instance.GetType() == type)
+					{
+						Debug.Log("workable.Value[EfficiencyMultiplier]: " + workable.Value["EfficiencyMultiplier"]);
+						__result = workable.Value["EfficiencyMultiplier"];
+					}
+				}
+			}
+			*/
+
+			//Debug.Log("__instance.GetType(): " + __instance.GetType());
+
+		}		
+	}
+
+	[HarmonyPatch(typeof(Workable), "GetAttributeExperienceMultiplier", new Type[] {  })]
+	internal static class WorkableEditorMod_Workable_GetAttributeExperienceMultiplier
+	{
+		private static void Postfix(Workable __instance, ref float __result)
+		{
+			if (!WorkableEditorConfig.Instance.Enabled) return;
+			//Debug.Log(" === WorkEditorMod_Constructable_GetAttributeExperienceMultiplier === " + __instance.GetType().ToString());
+
+			__result = WorkableEditorUtils.GetMultiplier("AttributeExperienceMultiplier", __result, __instance);			
+		}
+	}
+
+	[HarmonyPatch(typeof(Workable), "GetSkillExperienceMultiplier", new Type[] {  })]
+	internal static class WorkableEditorMod_Workable_GetSkillExperienceMultiplier
+	{
+		private static void Postfix(Workable __instance, ref float __result)
+		{
+			if (!WorkableEditorConfig.Instance.Enabled) return;
+			//Debug.Log(" === WorkEditorMod_Constructable_GetSkillExperienceMultiplier === " + __instance.GetType().ToString());
+
+			__result = WorkableEditorUtils.GetMultiplier("GetSkillExperienceMultiplier", __result, __instance);
+		}
+	}
+
+
+	/*
+    [HarmonyPatch(typeof(Database.AttributeConverters), "Create", new Type[] { typeof(string), typeof(string), typeof(string), typeof(Klei.AI.Attribute), typeof(float), typeof(float), typeof(IAttributeFormatter) })]
+    internal class InstantDigAndBuildMod
+    {
+        private static bool Prefix(Database.AttributeConverters __instance, string id, string name, string description, Klei.AI.Attribute attribute, ref float multiplier, float base_value, IAttributeFormatter formatter)
+        {
+            //Debug.Log(" === GetEfficiencyMultiplier InstantDigAndBuildMod loaded === " + id + " "+multiplier);
+            if (id.Equals("ConstructionSpeed") || id.Equals("DiggingSpeed"))
+            {
+                multiplier = 100.0f;
+            }
+            return true;
+        }
+    }
+    */
+}
---
+++ Source/WorkableEditor/WorkableEditorUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Harmony;
+
+namespace WorkableEditorMod
+{
+
+	class WorkableEditorUtils
+	{
+		public static List<Type> WorkableSubTypes;
+		public static List<Type> WorkableConfigTypes;
+
+
+		static WorkableEditorUtils()
+		{
+			WorkableSubTypes = FindAllDerivedTypes(typeof(Workable));
+			WorkableConfigTypes = new List<Type>();
+
+			foreach (var workable in WorkableEditorConfig.Instance.Workables)
+			{
+				Type type = AccessTools.TypeByName(workable.Key);
+				//Debug.Log("type: " + type);
+				if (WorkableEditorUtils.WorkableSubTypes.Contains(type))
+				{
+					WorkableConfigTypes.Add(type);
+				}
+			}
+		}
+
+		public static float GetMultiplier(string name, float defaultValue, Workable instance)
+		{
+
+			if (WorkableEditorUtils.WorkableConfigTypes.Contains(instance.GetType()))
+			{
+				//Debug.Log("__instance.GetType().FullName: " + instance.GetType().FullName);
+				var workable = WorkableEditorConfig.Instance.Workables[instance.GetType().FullName];
+				//Debug.Log("workable: " + workable);
+				//Debug.Log("workable[" + name + "]: " + workable[name]);
+				if (workable.ContainsKey(name))
+				{
+					return workable[name];
+				}
+			}
+			return defaultValue;
+		}
+
+		public static List<Type> FindAllDerivedTypes(Type type)
+		{
+			var derivedTypes = (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+								from assemblyType in domainAssembly.GetTypes()
+								where type.IsAssignableFrom(assemblyType)
+								select assemblyType);
+			return derivedTypes.ToList();
+		}
+
+
+	}
+
+}
