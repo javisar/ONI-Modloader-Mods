c1db213f05827296280b78b18edf4806dc73bcb3
javisar <javisar@gmail.com>
2019-05-17 13:34:26 +0200

    Added BetterModLoadLogs

---
+++ Source/BetterModLoadLogs/BetterModLoadLogs.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Harmony;
+
+namespace BetterModLoadLogs
+{
+	[HarmonyPatch(typeof(KMod.Manager), MethodType.Constructor)]
+	internal class BetterModLoadLogs_KMod_Manager_Constructor
+	{
+		private static void Postfix(KMod.Manager __instance)
+		{
+			Debug.Log(" === BetterModLoadLogs_Game_OnPrefabInit Postfix === ");
+			Assembly design = System.Reflection.Assembly.GetAssembly(typeof(KMod.Mod));
+			Type designHost = design.GetType("KMod.DLLLoader");
+			var original = designHost.GetMethod("LoadDLLs");
+			var transpiler = typeof(BetterModLoadLogs_KMod_Manager_Constructor).GetMethod("Transpiler");
+			HarmonyInstance.Create("BetterModLoadLogs_KMod_Manager_Constructor").Patch(original, null, null, new HarmonyMethod(transpiler));
+
+		}
+
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+		{
+			//Debug.Log(" === BetterModLoadLogs_Game_OnPrefabInit Transpiler === ");
+			List<CodeInstruction> instructions = new List<CodeInstruction>(instr);
+			
+			int idxAnchor = instructions.LastIndexOf(new CodeInstruction(OpCodes.Pop));
+			if (idxAnchor == -1)
+			{
+				Debug.Log("Could not find Page_ModsConfig.DoWindowContents transpiler anchor - not injecting code");
+				//return instructions;
+			}
+
+
+			var index = -1;
+			List<CodeInstruction> code = instr.ToList();
+			foreach (CodeInstruction codeInstruction in code)
+			{
+				index++;
+
+				if (idxAnchor >= 0
+					&& codeInstruction.opcode == OpCodes.Pop
+					&& index == idxAnchor)
+				{
+					Debug.Log(" === Transpiler applied === ");
+					codeInstruction.opcode = OpCodes.Call;
+					codeInstruction.operand = AccessTools.Method(typeof(Debug), nameof(Debug.LogException));					
+				}
+				else
+				{
+					yield return codeInstruction;
+				}
+			}			
+
+		}
+	}
+
+	/*
+	[HarmonyPatch(Assembly.LoadFile("").GetType("System.ComponentModel.Design.DesignerHost"), "LoadDLL")]
+	internal class SuperMinerMod_WorldDamage_OnDigComplete
+	{
+
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+		{
+			//Debug.Log(" === SuperMinerMod_WorldDamage_OnDigComplete Transpiler === ");
+
+			List<CodeInstruction> code = instr.ToList();
+			foreach (CodeInstruction codeInstruction in code)
+			{
+				if (codeInstruction.opcode == OpCodes.Ldc_R4
+					&& (float)codeInstruction.operand == (float)0.5)
+				{
+					Debug.Log(" === Transpiler applied === ");
+					codeInstruction.operand = 1.0f;
+				}
+				yield return codeInstruction;
+			}
+		}
+
+	}
+	*/
+}
---
+++ Source/BetterModLoadLogs/DLLLoader.cs
@@ -0,0 +1,128 @@
+﻿// KMod.DLLLoader
+using Harmony;
+using KMod;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+/*
+namespace KMod
+{
+	internal static class DLLLoader
+	{
+		private const string managed_path = "Managed";
+
+		public static bool LoadUserModLoaderDLL()
+		{
+			try
+			{
+				string path = Path.Combine(Path.Combine(Application.dataPath, "Managed"), "ModLoader.dll");
+				if (!File.Exists(path))
+				{
+					return false;
+				}
+				Assembly assembly = Assembly.LoadFile(path);
+				if (assembly == null)
+				{
+					return false;
+				}
+				Type type = assembly.GetType("ModLoader.ModLoader");
+				if (type == null)
+				{
+					return false;
+				}
+				MethodInfo method = type.GetMethod("Start");
+				if (method == null)
+				{
+					return false;
+				}
+				method.Invoke(null, null);
+				Debug.Log("Successfully started ModLoader.dll");
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Debug.Log(ex.ToString());
+			}
+			return false;
+		}
+
+		public static bool LoadDLLs(string path)
+		{
+			try
+			{
+				if (Testing.dll_loading == Testing.DLLLoading.Fail)
+				{
+					return false;
+				}
+				if (Testing.dll_loading == Testing.DLLLoading.UseModLoaderDLLExclusively)
+				{
+					return false;
+				}
+				Debug.LogFormat("Using built-in mod system...looking for DLL mods in {0}", Manager.GetDirectory());
+				DirectoryInfo directoryInfo = new DirectoryInfo(path);
+				if (!directoryInfo.Exists)
+				{
+					return false;
+				}
+				List<Assembly> list = new List<Assembly>();
+				FileInfo[] files = directoryInfo.GetFiles();
+				foreach (FileInfo fileInfo in files)
+				{
+					if (fileInfo.Name.ToLower().EndsWith(".dll"))
+					{
+						Debug.Log($"Loading MOD dll: {fileInfo.Name}");
+						Assembly assembly = Assembly.LoadFrom(fileInfo.FullName);
+						if (assembly != null)
+						{
+							list.Add(assembly);
+						}
+					}
+				}
+				if (list.Count == 0)
+				{
+					return false;
+				}
+				HarmonyInstance harmonyInstance = HarmonyInstance.Create($"OxygenNotIncluded_v{0}.{1}");
+				if (harmonyInstance != null)
+				{
+					foreach (Assembly item in list)
+					{
+						harmonyInstance.PatchAll(item);
+					}
+				}
+				Type[] types = new Type[0];
+				Type[] types2 = new Type[1]
+				{
+				typeof(string)
+				};
+				object[] parameters = new object[1]
+				{
+				path
+				};
+				foreach (Assembly item2 in list)
+				{
+					Type[] types3 = item2.GetTypes();
+					foreach (Type type in types3)
+					{
+						if (type != null)
+						{
+							type.GetMethod("OnLoad", types)?.Invoke(null, null);
+							type.GetMethod("OnLoad", types2)?.Invoke(null, parameters);
+						}
+					}
+				}
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Debug.LogException(ex);
+				return false;
+			}
+		}
+	}
+
+}
+*/
